1268030d87965826c46fa4202f3dbbecac5b2535
PurinPGR <godofpurin03@gmail.com>
2025-10-21 22:42:09 +0900

    的の動きを一通り実装

--- Assets/Programs/Target_L3.cs
+++ Assets/Programs/Target_L3.cs
@@ -2,30 +2,48 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 public class Target_L3 : MonoBehaviour
 {
+    public float breakCount;
+    public GameObject breakEffect;
+    public UnityEngine.Vector3 targetPosition = new UnityEngine.Vector3(5.0f, 2.0f, -3.0f);
+    public float speed = 2.0f;
 
-    float speed;
+    public float scoreValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        speed = UnityEngine.Random.Range(1.0f, 2.0f);
+       
     }
-
     // Update is called once per frame
     void Update()
+    {  
+        transform.position = UnityEngine.Vector3.Lerp
+        (transform.position, targetPosition, speed * Time.deltaTime);
+    }
+    
+
+    void OnCollisionEnter(Collision collision)
     {
-        
-        float wave = Mathf.Sin(Time.time * speed);
-        transform.position = new UnityEngine.Vector3(4.6f + -speed * Time.time, 2.0f + wave, -49f);
+        breakCount--;
+        if (breakCount == 0)
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            GenerateEffect();
+        }
+    }
+    
+    void GenerateEffect()
+    {
+        GameObject effect = Instantiate(breakEffect) as GameObject;
+        effect.transform.position = gameObject.transform.position;
     }
 
-    void OnBecameInvisible() 
-    {
-    Destroy(this.gameObject); // オブジェクトを破棄
-    }
 
 }
